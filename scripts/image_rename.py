ef454a65d319b2159772a20d038e9e0883d1e450
Rishi979580 <futureway.in@gmail.com>
2025-07-19 18:31:48 +0530

    first commit

---
+++ scripts/image_rename.py
@@ -0,0 +1,24 @@
+import os
+
+# Folder path
+folder_path = r'D:\YoutubePoster\input\wallpapers'
+
+# Change working directory
+os.chdir(folder_path)
+
+# List all files and sort (optional)
+files = sorted(os.listdir())
+
+# Rename images
+count = 1
+for filename in files:
+    # Split filename and extension
+    name, ext = os.path.splitext(filename)
+    
+    # Skip folders (only files)
+    if os.path.isfile(filename):
+        new_name = f"{count}{ext}"
+        os.rename(filename, new_name)
+        count += 1
+
+print("Renaming Complete!")
